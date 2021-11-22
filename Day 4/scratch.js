ddd7a07a63e18954793b7073c68fb9d5affadc6f
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-11-22 20:53:21 +0000

    adding scratch file

--- Day 4/scratch.js
+++ Day 4/scratch.js
@@ -1,88 +1,19 @@
-let formatter = new Intl.NumberFormat("en-UK", {
-  style: "currency",
-  currency: "GBP",
-});
-
-const coffeeShop = {
-  branch: "Liverpool",
-  drinks: [
-    {
-      menuItem: 1,
-      drink: "tea",
-      price: 2.5,
-    },
-    {
-      menuItem: 2,
-      drink: "coffee",
-      price: 3.99,
-    },
-    {
-      menuItem: 3,
-      drink: "fanta",
-      price: 1.5,
-    },
-  ],
-  food: [
-    {
-      menuItem: 4,
-      food: "beans on toast",
-      price: 3,
-    },
-    {
-      menuItem: 5,
-      food: "cheese panini",
-      price: 3.5,
-    },
-    {
-      menuItem: 6,
-      food: "warldorf salad",
-      price: 8,
-    },
-  ],
-  processOrder(order, type) {
-    // loop the order
-    let cost = 0;
-    let ticket = `
-      Ticket - Drinks
-      --------------------------`;
-    // loop the food menu
-    for (let i = 0; i < order.length; i++) {
-      const found = this.type.find((o) => o.menuItem === order[i]);
-      if (typeof found !== "undefined") {
-        cost += found.price;
-
-        if ((type = "drinks")) {
-          ticket +=
-            `
-            ${found.drink}: ${formatter.format(found.price)}` + "\n";
-        } else {
-          ticket +=
-            `
-            ${found.food}: ${formatter.format(found.price)}` + "\n";
-        }
-      } else {
-        ticket +=
-          `
-          Something went wrong!` + "\n";
-      }
-    }
-    ticket += `
-      --------------------------
-               Total Cost: ${formatter.format(cost)}
-      `;
-    return ticket;
+let coffeeShop = {
+  branch: "City Centre",
+  drinksMenu: {
+    coffee: 2,
+    tea: 3,
   },
-
-  drinksOrdered(order) {
-    const returnValue = this.processOrder(order, this.drinks);
-    return returnValue;
+  foodMenu: {
+    sandwich: 5,
+    pasty: 4,
   },
-  foodOrdered(order) {
-    const returnValue = this.processOrder(order, this.food);
-    return returnValue;
+  aggregate() {
+    let allItems = [
+      ...Object.keys(this.drinksMenu),
+      ...Object.keys(this.foodMenu),
+    ];
+    console.log(allItems);
   },
 };
-
-//let myFoodOrder = [8, 4, 5, 9];
-let myDrinksOrder = [1, 2];
-console.log(coffeeShop.drinksOrdered(myDrinksOrder));
+coffeeShop.aggregate();
